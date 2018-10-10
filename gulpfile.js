76220517445c5fa4ad826a83d991aaf956b55e85
EC2 Default User <ec2-user@ip-172-31-65-241.ec2.internal>
2018-10-10 21:18:31 +0000

    Added Gulp Sass

--- gulpfile.js
+++ gulpfile.js
@@ -15,5 +15,22 @@ gulp.task('styles', function () {
 });
 
 
-gulp.task('default', ['scripts', 'styles']);
 
+//Adding gulp-sass
+
+var sass = require('gulp-sass');
+ 
+gulp.task('sass', function () {
+  return gulp.src('src/scss/*.scss')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(gulp.dest('dist/styles'));
+});
+ 
+gulp.task('sass:watch', function () {
+  gulp.watch('src/scss/**/*.scss', ['sass']);
+});
+
+
+
+
+gulp.task('default', ['scripts', 'styles', 'sass']);
