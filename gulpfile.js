faa159ce9b84123b41f266a30c03032d1e46e325
EC2 Default User <ec2-user@ip-172-31-65-241.ec2.internal>
2018-10-11 00:27:36 +0000

    Added gulp packages

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,18 @@
 var gulp = require('gulp'), 
-uglify = require ('gulp-uglify');
+uglify = require ('gulp-uglify'), 
+sass = require('gulp-sass'),
+jquery = require('jquery'),
+concat = require("gulp-concat");
 
 // Scripts Task
 // Uglifies
 
 gulp.task('scripts', function () {
-    gulp.src('src/js/*.js')
+    return gulp.src([
+      "node_modules/jquery/dist/jquery.min.js", 
+      "src/js/*.js"
+      ])
+    .pipe(concat('main.js'))
     .pipe(uglify())
     .pipe(gulp.dest('dist/scripts'))
 });
@@ -15,22 +22,27 @@ gulp.task('styles', function () {
 });
 
 
-
 //Adding gulp-sass
 
-var sass = require('gulp-sass');
- 
 gulp.task('sass', function () {
-  return gulp.src('src/scss/*.scss')
-    .pipe(sass().on('error', sass.logError))
+  return gulp.src([
+    'node_modules/bootstrap/scss/bootstrap.scss', 
+    'src/scss/main.scss'
+    ])
+    
+    .pipe(sass({
+                outputStyle: "compressed"}).on('error', sass.logError))
+    .pipe(concat('main.css'))
     .pipe(gulp.dest('dist/styles'));
 });
- 
+
+
 gulp.task('sass:watch', function () {
-  gulp.watch('src/scss/**/*.scss', ['sass']);
+  gulp.watch('src/scss/main.scss', ['sass']);
 });
 
 
+ 
 
 
 gulp.task('default', ['scripts', 'styles', 'sass']);
